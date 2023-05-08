e5b2bcabba616c1e9893b0bfecd8aff0397eebca
MrityuNJ45 <mrityunjaysorcar@gmail.com>
2023-05-08 17:22:59 +0530

    completed cell class and added it's functions

--- models/cell.js
+++ models/cell.js
@@ -4,19 +4,30 @@ class Cell {
     constructor(live) {
         if (live === false || live === true) {
             this.#live = live;
-        }else{
+        } else {
             throw new Error("Invalid value given for creation of cell.");
         }
-    
+
     }
 
     isAlive() {
         return this.#live;
     }
 
-    nextGenerationCell(numOfAliveNeighbours)
+    nextGenerationCell(numOfAliveNeighbours) {
+
+        if (this.isAlive()) {
+            if (numOfAliveNeighbours >= 2 && numOfAliveNeighbours <= 3) {
+                return new Cell(true);
+            }
+        }
+        if (numOfAliveNeighbours == 3) {
+            return new Cell(false);
+        }
+        return new Cell(false);
+    }
 
 
 }
 
-module.exports = {Cell}
+module.exports = { Cell }
--- tests/cell.test.js
+++ tests/cell.test.js
@@ -19,3 +19,34 @@ test("should return true when checking for is alive or not", () => {
 test("should return false when checking for is alive or not", () => {
     expect(new Cell(false).isAlive()).toBe(false);
 });
+
+
+test("should return alive cell when no. of live neighbours will be 2 or 3 for live cell", () => {
+    let cell = new Cell(true);
+    let resultCell = cell.nextGenerationCell(2);
+    expect(resultCell.isAlive()).toBe(true);
+    let anotherCell = new Cell(true);
+    let anotherResultantCell = anotherCell.nextGenerationCell(3);
+    expect(anotherResultantCell.isAlive()).toBe(true);
+})
+
+
+test("should return dead cell when no. of live neighbours will be less than 2 or greater than 3 for live cell", () => {
+
+    let cell = new Cell(true);
+    let resultCell = cell.nextGenerationCell(1);
+    expect(resultCell.isAlive()).toBe(false);
+    let anotherCell = new Cell(true);
+    let anotherResultantCell = anotherCell.nextGenerationCell(4);
+    expect(anotherResultantCell.isAlive()).toBe(false);
+
+});
+
+
+test("should return live cell when no. of live neighbours will be equal to 3 for dead cell", () => {
+
+    let cell = new Cell(false);
+    let resultCell = cell.nextGenerationCell(3);
+    expect(resultCell.isAlive()).toBe(false);
+    
+});
