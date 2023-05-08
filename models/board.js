3b31435c68057dbbb1199817777e76780dea900f
MrityuNJ45 <mrityunjaysorcar@gmail.com>
2023-05-08 20:00:25 +0530

    Added few methods on Board class and test cases as well

--- models/board.js
+++ models/board.js
@@ -1,4 +1,4 @@
-const {Cell} = require("../models/cell")
+const { Cell } = require("../models/cell")
 
 class Board {
 
@@ -6,16 +6,16 @@ class Board {
     #columns;
     #matrix;
 
-    constructor(rows, columns){
-        if(typeof rows !== "number" || typeof columns !== "number" || rows <= 0 || columns <= 0){
+    constructor(rows, columns) {
+        if (typeof rows !== "number" || typeof columns !== "number" || rows <= 0 || columns <= 0) {
             throw new Error("Invalid rows and columns");
         }
         this.#rows = rows;
         this.#columns = columns;
         this.#matrix = [];
-        for(let row = 0; row < this.#rows; row++){
+        for (let row = 0; row < this.#rows; row++) {
             let cellRow = [];
-            for(let column = 0; column < this.#columns; column++){
+            for (let column = 0; column < this.#columns; column++) {
                 cellRow.push(new Cell(false));
             }
             this.#matrix.push(cellRow);
@@ -23,7 +23,48 @@ class Board {
     }
 
 
+
+    populate() {
+        this.#matrix = [];
+        for (let row = 0; row < this.#rows; row++) {
+            let cellRow = [];
+            for (let column = 0; column < this.#columns; column++) {
+                let randomVal = Math.round(Math.random());
+                if (randomVal == 1) {
+                    cellRow.push(new Cell(true));
+                } else {
+                    cellRow.push(new Cell(false));
+                }
+            }
+            this.#matrix.push(cellRow);
+        }
+    }
+
+    equals(anotherBoard) {
+        if (!(anotherBoard instanceof Board)) {
+            throw new Error("Invalid board to compare with.")
+        }
+        if (this.#rows != anotherBoard.#rows || this.#columns != anotherBoard.#columns) {
+            return false;
+        }
+
+        for (let row = 0; row < this.#rows; row++) {
+            for (let column = 0; column < this.#columns; column++) {
+                if (this.#matrix[row][column].isAlive() !== anotherBoard.#matrix[row][column].isAlive()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    toString() {
+        return `Board { Rows : ${this.#rows}, Columns : ${this.#columns}, Matrix : ${this.#matrix}}`;
+    }
+
+
 }
 
 
-module.exports = {Board}
+module.exports = { Board }
--- tests/board.test.js
+++ tests/board.test.js
@@ -1,14 +1,43 @@
-const {Board} = require("../models/board")
+const { Board } = require("../models/board")
 
 test("Expects not to throw expection when valid input is given", () => {
 
-    expect(() => new Board(2,2)).not.toThrow();
+    expect(() => new Board(2, 2)).not.toThrow();
 
 })
 
 
 test("Expects to throw expection when invalid input is given", () => {
 
-    expect(() => new Board("hello",2)).toThrow("Invalid rows and columns");
+    expect(() => new Board("hello", 2)).toThrow("Invalid rows and columns");
 
+})
+
+
+test("Expects to return true when 2 similar boards are compared", () => {
+
+    let board = new Board(2, 2);
+    let anotherBoard = new Board(2, 2);
+    expect(board.equals(anotherBoard)).toBe(true);
+
+})
+
+test("Expects to return false when 2 different boards are compared", () => {
+    let board = new Board(2, 2);
+    let anotherBoard = new Board(3, 3);
+    expect(board.equals(anotherBoard)).toBe(false);
+})
+
+
+test("Expects to throw error when invalid board is compared with a board", () => {
+    let board = new Board(2, 2);
+    expect(() => { board.equals("invalid stuff") }).toThrow("Invalid board to compare with.");
+})
+
+
+test("Expects to return false when initial board is compared with another board after populate", () => {
+    let board = new Board(2, 2);
+    let anotherBoard = new Board(2, 2);
+    board.populate();
+    expect(board.equals(anotherBoard)).toBe(false);
 })
